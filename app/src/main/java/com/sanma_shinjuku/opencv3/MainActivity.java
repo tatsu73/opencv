1ac095dac4f0b155b95c87d35d604b4622576f7b
tatsu73 <tatsuro.saito73@gmail.com>
2016-01-05 04:37:21 +0900

    cahnge mani

--- app/src/main/java/com/sanma_shinjuku/opencv3/MainActivity.java
+++ app/src/main/java/com/sanma_shinjuku/opencv3/MainActivity.java
@@ -3,11 +3,30 @@ package com.sanma_shinjuku.opencv3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.core.Mat;
+
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return null;
     }
 }
