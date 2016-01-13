aaca730032bbeb82d89e7a49030d4db496fc2dfa
tatsu73 <tatsuro.saito73@gmail.com>
2016-01-13 11:55:20 +0900

    test code

--- app/src/main/java/com/sanma_shinjuku/opencv3/MainActivity.java
+++ app/src/main/java/com/sanma_shinjuku/opencv3/MainActivity.java
@@ -2,31 +2,96 @@ package com.sanma_shinjuku.opencv3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
+
+    // カメラビューのインスタンス
+    // CameraBridgeViewBase は JavaCameraView/NativeCameraView のスーパークラス
+    private CameraBridgeViewBase mCameraView;
+    private Mat mOutputFrame;
+
+
+    // ライブラリ初期化完了後に呼ばれるコールバック (onManagerConnected)
+    // public abstract class BaseLoaderCallback implements LoaderCallbackInterface
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                // 読み込みが成功したらカメラプレビューを開始
+                case LoaderCallbackInterface.SUCCESS:
+                    mCameraView.enableView();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // カメラビューのインスタンスを変数にバインド
+        mCameraView = (CameraBridgeViewBase) findViewById(R.id.camera_view);
+        // リスナーの設定 (後述)
+        mCameraView.setCvCameraViewListener(this);
     }
 
     @Override
+    public void onPause(){
+        if (mCameraView != null) {
+            mCameraView.disableView();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 非同期でライブラリの読み込み/初期化を行う
+        // static boolean initAsync(String Version, Context AppContext, LoaderCallbackInterface Callback)
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mCameraView != null) {
+            mCameraView.disableView();
+        }
+    }
+
+
+
+    @Override
     public void onCameraViewStarted(int width, int height) {
-        
+        mOutputFrame = new Mat(height, width, CvType.CV_8UC1);
     }
 
     @Override
     public void onCameraViewStopped() {
-
+        mOutputFrame.release();
     }
 
+    //画像処理はここに
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        return null;
+        // Cannyフィルタをかける
+        Imgproc.Canny(inputFrame.gray(), mOutputFrame, 80, 100);
+        // ビット反転
+        Core.bitwise_not(mOutputFrame, mOutputFrame);
+        return mOutputFrame;
     }
 }
